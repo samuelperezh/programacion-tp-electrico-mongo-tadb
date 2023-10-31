260b6ca8e24bd3aa8e15871799ac2d156a27e0ab
Luisa Flórez <luisa.florezm@upb.edu.co>
2023-10-31 11:25:02 -0500

    Script Creación del modelo

--- datos/script-mongodb-programacion-tp-electrico.sql
+++ datos/script-mongodb-programacion-tp-electrico.sql
@@ -43,100 +43,107 @@ db.createCollection("utilizacion_cargadores");
 db.createCollection("operacion_autobuses");
 
 -- Creamos las colecciones usando un json schema para validación
+// Colección: Cargadores
 db.createCollection("cargadores", {
    validator: {
       $jsonSchema: {
-         bsonType: 'object',
-         title: 'Cargadores de los buses',
-         required: ['nombre_cargador'],
+         bsonType: "object",
+         title: "Cargadores de Buses del Transporte Público Eléctrico",
+         required: ["nombre_cargador"],
          properties: {
             nombre_cargador: {
-               bsonType: 'string',
-               description: "'nombre_cargador' debe ser una cadena de caracteres y no puede ser nulo"
+               bsonType: "string",
+               description: "'nombre_cargador' Debe ser una cadena de caracteres y no puede ser nulo"
             }
          }
       }
    }
 });
 
+// Colección: Autobuses
 db.createCollection("autobuses", {
    validator: {
       $jsonSchema: {
-         bsonType: 'object',
-         title: 'Autobuses del sistema de transporte público eléctrico',
-         required: ['nombre_autobus'],
+         bsonType: "object",
+         title: "Autobuses del Transporte Público Eléctrico",
+         required: ["nombre_autobus"],
          properties: {
             nombre_autobus: {
-               bsonType: 'string',
-               description: "'nombre_autobus' debe ser una cadena de caracteres y no puede ser nulo"
+               bsonType: "string",
+               description: "'nombre_autobus' Debe ser una cadena de caracteres y no puede ser nulo"
             }
          }
       }
    }
 });
 
+// Colección: Horarios
 db.createCollection("horarios", {
    validator: {
       $jsonSchema: {
-         bsonType: 'object',
-         title: 'Horarios de operación',
-         required: ['id', 'horario_pico'],
+         bsonType: "object",
+         title: "Horarios de Operación del Transporte Público Eléctrico",
+         required: ["id", "horario_pico"],
          properties: {
             id: {
-               bsonType: 'int',
-               description: "'id' debe ser un entero y no puede ser nulo"
+               bsonType: "int",
+               description: "'id' Debe ser un número entero y no puede ser nulo"
             },
             horario_pico: {
-               description: "'horario_pico' debe ser un booleano y no puede ser nulo"
+               bsonType: "bool",
+               description: "'horario_pico' Debe ser un valor booleano (true o false) y no puede ser nulo"
             }
          }
       }
    }
 });
 
+// Colección: Utilización de Cargadores
 db.createCollection("utilizacion_cargadores", {
    validator: {
       $jsonSchema: {
-         bsonType: 'object',
-         title: 'Utilización de cargadores de buses',
-         required: ['cargador_id', 'autobus_id', 'horario_id'],
+         bsonType: "object",
+         title: "Utilización de Cargadores de Buses del Transporte Público Eléctrico",
+         required: ["cargador_id", "autobus_id", "horario_id"],
          properties: {
             cargador_id: {
-               bsonType: 'int',
-               description: "'cargador_id' debe ser un entero y no puede ser nulo"
+               bsonType: "int",
+               description: "'cargador_id' Debe ser un número entero y no puede ser nulo"
             },
             autobus_id: {
-               bsonType: 'int',
-               description: "'autobus_id' debe ser un entero y no puede ser nulo"
+               bsonType: "int",
+               description: "'autobus_id' Debe ser un número entero y no puede ser nulo"
             },
             horario_id: {
-               bsonType: 'int',
-               description: "'horario_id' debe ser un entero y no puede ser nulo"
+               bsonType: "int",
+               description: "'horario_id' Debe ser un número entero y no puede ser nulo"
             }
          }
       }
    }
 });
 
+// Colección: Operación de Autobuses
 db.createCollection("operacion_autobuses", {
    validator: {
       $jsonSchema: {
-         bsonType: 'object',
-         title: 'Operación de autobuses del sistema de transporte público eléctrico',
-         required: ['autobus_id', 'horario_id'],
+         bsonType: "object",
+         title: "Operación de Autobuses del Transporte Público Eléctrico",
+         required: ["autobus_id", "horario_id"],
          properties: {
             autobus_id: {
-               bsonType: 'int',
-               description: "'autobus_id' debe ser un entero y no puede ser nulo"
+               bsonType: "int",
+               description: "'autobus_id' Debe ser un número entero y no puede ser nulo"
             },
             horario_id: {
-               bsonType: 'int',
-               description: "'horario_id' debe ser un entero y no puede ser nulo"
+               bsonType: "int",
+               description: "'horario_id' Debe ser un número entero y no puede ser nulo"
             }
          }
       }
    }
 });
+
 
 -- ***************************************************
 -- Consultas de apoyo para implementar el repositorio
